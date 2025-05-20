971a9bdd4f7e1087e8e4dca24c749796a92f1690
Christopher Walton <chriswalton@cwsoftware.biz>
2025-05-20 13:51:02 +0700

    fixing things

--- apps/site/src/app/pages/account/components/transaction-grid/transaction-grid.component.ts
+++ apps/site/src/app/pages/account/components/transaction-grid/transaction-grid.component.ts
@@ -1,5 +1,5 @@
 import { Component, input } from '@angular/core';
-import { GridModule, SortService } from '@syncfusion/ej2-angular-grids';
+import { GridModule } from '@syncfusion/ej2-angular-grids';
 import type { Transaction } from '@waldrick-bank/shared';
 
 @Component({
@@ -7,7 +7,6 @@ import type { Transaction } from '@waldrick-bank/shared';
   selector: 'app-transaction-grid',
   templateUrl: './transaction-grid.component.html',
   imports: [GridModule],
-  providers: [SortService],
 })
 export class TransactionGridComponent {
   readonly transactions = input.required<Transaction[] | undefined | null>();
